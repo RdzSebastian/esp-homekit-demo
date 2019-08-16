1ad11ffb69e26c11f82f1efaeb367fde161b3602
RdzSebastian <RdzSebastian@gmail.com>
2019-08-15 21:45:06 -0300

    Rollback cuarto 2 relay

--- examples/cuarto/cuarto.c
+++ examples/cuarto/cuarto.c
@@ -138,87 +138,6 @@ void relay2_on_set(homekit_value_t value) {
 }
 
 
-// ----------------------------------------------------------   Logica boton 1   -----------------------------------------------------
-
-typedef struct _button {
-    uint8_t gpio_num;
-    button_callback_fn callback;
-
-    uint16_t debounce_time;
-    uint16_t long_press_time;
-    uint16_t double_press_time;
-
-    uint8_t press_count;
-    ETSTimer press_timer;
-    uint32_t last_press_time;
-    uint32_t last_event_time;
-
-    struct _button *next;
-} button_t;
-
-button_t *buttons = NULL;
-
-
-static button_t *button_find_by_gpio(const uint8_t gpio_num) {
-    button_t *button = buttons;
-    while (button && button->gpio_num != gpio_num)
-        button = button->next;
-
-    return button;
-}
-
-void button1_identify(homekit_value_t _value) {
-    printf("Button identify\n");
-}
-
-
-int button_create(const uint8_t gpio_num, button_callback_fn callback) {
-    button_t *button = button_find_by_gpio(gpio_num);
-    if (button)
-        return -1;
-
-    button = malloc(sizeof(button_t));
-    memset(button, 0, sizeof(*button));
-    button->gpio_num = gpio_num;
-    button->callback = callback;
-
-    // times in milliseconds
-    button->debounce_time = 50;
-    button->long_press_time = 1000;
-    button->double_press_time = 500;
-
-    button->next = buttons;
-    buttons = button;
-
-    gpio_set_pullup(button->gpio_num, true, true);
-    gpio_set_interrupt(button->gpio_num, GPIO_INTTYPE_EDGE_ANY, button_intr_callback);
-
-    sdk_os_timer_disarm(&button->press_timer);
-    sdk_os_timer_setfn(&button->press_timer, button_timer_callback, button);
-
-    return 0;
-}
-
-
-// ----------------------------------------------------------   Logica boton 2   -----------------------------------------------------
-
-void button2_identify(homekit_value_t _value) {
-    printf("Button identify\n");
-}
-
-
-void button2_init() {
-    gpio_enable(button2, GPIO_INPUT);
-    relay2_write(state2);
-}
-
-
-void button2_on_set(homekit_value_t value) {
-
-    state2 = value.bool_value;
-    relay2_write(state2);
-}
-
 // ------------------------------------------------   Configuracion del server de Homekit   ------------------------------------------
 
 homekit_accessory_t *accessories[] = {
@@ -262,38 +181,6 @@ homekit_accessory_t *accessories[] = {
         }),
         NULL
     }),
-    HOMEKIT_ACCESSORY(.id=3, .category=homekit_accessory_category_programmable_switch, .services=(homekit_service_t*[]){
-        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Cuarto"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Estonian Port"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "ASD123"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "C.U.C.A"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, button1_identify),
-            NULL
-        }),
-        HOMEKIT_SERVICE(STATELESS_PROGRAMMABLE_SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            &button_event,
-            NULL
-        }),
-        NULL
-    }),
-    HOMEKIT_ACCESSORY(.id=4, .category=homekit_accessory_category_programmable_switch, .services=(homekit_service_t*[]){
-        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Cuarto"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Estonian Port"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "ASD123"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "C.U.C.A"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, button2_identify),
-            NULL
-        }),
-        HOMEKIT_SERVICE(STATELESS_PROGRAMMABLE_SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            &button_event,
-            NULL
-        }),
-        NULL
-    }),
     
     NULL
 };
@@ -313,14 +200,5 @@ void user_init(void) {
     wifi_init();
     relay1_init();
     relay2_init();
-
-    if (button_create(button1, button_callback)) {
-        printf("Failed to initialize button\n");
-    }
-    
-    if (button_create(button2, button_callback)) {
-        printf("Failed to initialize button\n");
-    }
-
     homekit_server_init(&config);
 }
